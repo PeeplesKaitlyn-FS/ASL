c06d9a8d72c55ac4f980cf447c017e18f646fc4f
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-11 14:37:29 -0500

    Update app.js

--- src/node/app.js
+++ src/node/app.js
@@ -1,2 +1,9 @@
+const http = require('http');
 const date = new Date();
-console.log(`Hello ASL! ${date.toLocaleString()}`);
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`Hello ASL! ${date.toLocaleString()}`);
+}).listen(3001, () => {
+    console.log(`Hello ASL! ${date.toLocaleString()}`);
+});
