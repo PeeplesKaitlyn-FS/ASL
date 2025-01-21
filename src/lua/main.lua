4de9caff4ed5c10cf1302c4ddcc478b4bbb08c84
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-01-21 14:00:18 -0500

    Update main.lua

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -1,2 +1,4 @@
 local date = os.date("*t")
-print("Hello ASL! " .. os.date("%Y-%m-%d %H:%M:%S"))
+
+print("Hello, world! The current date and time is:")
+print(string.format("%04d-%02d-%02d %02d:%02d:%02d", date.year, date.month, date.day, date.hour, date.min, date.sec))
