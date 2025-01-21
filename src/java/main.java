d08dce6a30d6cc71aaea508ad0b59abc3901ffc1
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-01-21 14:00:14 -0500

    Update main.java

--- src/java/main.java
+++ src/java/main.java
@@ -1,26 +1,11 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.net.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(3000);
-        System.out.println("Server started. Listening for incoming connections...");
-
-        while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Incoming connection from " + socket.getInetAddress());
-
-            LocalDateTime date = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String message = "Hello ASL! " + date.format(formatter);
-
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(message.getBytes());
-            outputStream.close();
-
-            socket.close();
-        }
+    public static void main(String[] args) {
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String message = "Hello ASL! " + date.format(formatter);
+        System.out.println(message);
     }
 }
